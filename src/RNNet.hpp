3b3083913824c21bcc50787103f74fb4147f1e90
huiwenliuge <kkkk_l@sina.com>
2016-12-11 11:06:56 +0800

    NONE

--- src/RNNet.hpp
+++ src/RNNet.hpp
@@ -57,11 +57,11 @@ class RNNet{
         double** ro2h;
 
         double sigm(double x){
-            return 1.0/(1.0+exp(-x/1.0));
+            return 1.0/(1.0+exp(-x));
         }
 
         double dsigm(double x){
-            return sigm(x)*(1.0-sigm(x)/1.0)/1.0;
+            return sigm(x)*(1.0-sigm(x));
         }
         
         
